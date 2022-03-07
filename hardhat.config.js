324bf388a26c67f46a7d998b70558b2331948b5c
Ebube <okorieebube1@gmail.com>
2022-03-07 20:10:01 +0100

    Factory contract homework

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,7 @@
 require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-ethers");
 require("./tasks/PrintAccounts");
+require("hardhat-gas-reporter");
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
